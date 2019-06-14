f0bed8e924aa0c4dbcf292ca294b36836c6fa97a
Fidel Xu <fidel.xu@gmail.com>
2019-06-14 08:21:17 +0800

    fidel.xu

--- web/auth/oauth2/tokenstore.go
+++ web/auth/oauth2/tokenstore.go
@@ -2,13 +2,13 @@ package oauth2
 
 import (
 	"context"
-	"errors"
 
 	"github.com/fidelfly/fxgo/authx"
 	"github.com/fidelfly/fxgo/logx"
 	"github.com/micro/go-micro/client"
 	"gopkg.in/oauth2.v3"
 
+	"github.com/fidelfly/fxms/mskit/proto/base"
 	"github.com/fidelfly/fxms/srv/auth/proto/token"
 )
 
@@ -18,18 +18,15 @@ type tokenStore struct {
 }
 
 func NewTokenStore() *tokenStore {
-	client := token.NewTokenService("com.fxms.srv.auth", client.DefaultClient)
-	return &tokenStore{client: client, cache: authx.NewMemoryTokenStore()}
+	c := token.NewTokenService("com.fxms.srv.auth", client.DefaultClient)
+	return &tokenStore{client: c, cache: authx.NewMemoryTokenStore()}
 }
 
 // create and store the new token information
 func (s *tokenStore) Create(info oauth2.TokenInfo) error {
-	resp, err := s.client.Create(context.Background(), &token.TokenRequest{Token: token.NewTokenData(info)})
+	_, err := s.client.Create(context.Background(), &token.TokenRequest{Token: token.NewTokenData(info)})
 	if err != nil {
 		return err
-	}
-	if len(resp.ErrMessage) > 0 {
-		return errors.New(resp.ErrMessage)
 	}
 	logx.CaptureError(s.cache.Create(info))
 	return nil
@@ -41,14 +38,8 @@ func (s *tokenStore) RemoveByCode(code string) error {
 	if err != nil {
 		return err
 	}
-	res, err := s.client.RemoveByCode(context.Background(), &token.StringValue{Value: code})
-	if err != nil {
-		return err
-	}
-	if len(res.ErrMessage) > 0 {
-		return errors.New(res.ErrMessage)
-	}
-	return nil
+	_, err = s.client.RemoveByCode(context.Background(), &base.StringValue{Value: code})
+	return err
 }
 
 // use the access token to delete the token information
@@ -57,14 +48,8 @@ func (s *tokenStore) RemoveByAccess(access string) error {
 	if err != nil {
 		return err
 	}
-	res, err := s.client.RemoveByAccess(context.Background(), &token.StringValue{Value: access})
-	if err != nil {
-		return err
-	}
-	if len(res.ErrMessage) > 0 {
-		return errors.New(res.ErrMessage)
-	}
-	return nil
+	_, err = s.client.RemoveByAccess(context.Background(), &base.StringValue{Value: access})
+	return err
 }
 
 // use the refresh token to delete the token information
@@ -73,14 +58,8 @@ func (s *tokenStore) RemoveByRefresh(refresh string) error {
 	if err != nil {
 		return err
 	}
-	res, err := s.client.RemoveByRefresh(context.Background(), &token.StringValue{Value: refresh})
-	if err != nil {
-		return err
-	}
-	if len(res.ErrMessage) > 0 {
-		return errors.New(res.ErrMessage)
-	}
-	return nil
+	_, err = s.client.RemoveByRefresh(context.Background(), &base.StringValue{Value: refresh})
+	return err
 }
 
 // use the authorization code for token information data
@@ -89,12 +68,9 @@ func (s *tokenStore) GetByCode(code string) (oauth2.TokenInfo, error) {
 	if err == nil && ti != nil {
 		return ti, nil
 	}
-	res, err := s.client.GetByCode(context.Background(), &token.StringValue{Value: code})
+	res, err := s.client.GetByCode(context.Background(), &base.StringValue{Value: code})
 	if err != nil {
 		return nil, err
-	}
-	if len(res.ErrMessage) > 0 {
-		return nil, errors.New(res.ErrMessage)
 	}
 	return token.NewTokenInfo(res.Token), nil
 }
@@ -105,12 +81,9 @@ func (s *tokenStore) GetByAccess(access string) (oauth2.TokenInfo, error) {
 	if err == nil && ti != nil {
 		return ti, nil
 	}
-	res, err := s.client.GetByAccess(context.Background(), &token.StringValue{Value: access})
+	res, err := s.client.GetByAccess(context.Background(), &base.StringValue{Value: access})
 	if err != nil {
 		return nil, err
-	}
-	if len(res.ErrMessage) > 0 {
-		return nil, errors.New(res.ErrMessage)
 	}
 	return token.NewTokenInfo(res.Token), nil
 }
@@ -121,12 +94,9 @@ func (s *tokenStore) GetByRefresh(refresh string) (oauth2.TokenInfo, error) {
 	if err == nil && ti != nil {
 		return ti, nil
 	}
-	res, err := s.client.GetByRefresh(context.Background(), &token.StringValue{Value: refresh})
+	res, err := s.client.GetByRefresh(context.Background(), &base.StringValue{Value: refresh})
 	if err != nil {
 		return nil, err
-	}
-	if len(res.ErrMessage) > 0 {
-		return nil, errors.New(res.ErrMessage)
 	}
 	return token.NewTokenInfo(res.Token), nil
 }
